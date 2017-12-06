854f965291719f0e9e855dbcb51c8b77c4958bac
Yaroslav Gribov <yaroslavgribov@yandex.ru>
2017-12-06 13:23:16 +0300

    fix config and babel preset

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,66 +1,64 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const extractSass = new ExtractTextPlugin({
-    filename: "[name].css"
-});
-
-var APP_NAME = 'main';
+  filename: '[name].css'
+})
 
 module.exports = {
-    entry: __dirname + '/entry.js',
-    devtool: 'source-map',
-    output: {
-      path: __dirname + '/assets/dist',
-      filename: 'main.js',
-    },
-    module: {
-      rules: [
+  entry: __dirname + '/entry.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/assets/dist',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        query: {
+          presets: ['env']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
           {
-              test: /(\.jsx|\.js)$/,
-              loader: 'babel-loader',
-              exclude: /(node_modules)/,
-              query: {
-                presets: ['es2015', 'react']
-              }
-          }, {
-              test: /\.css$/,
-              use: [
-                  { loader: 'style-loader'},
-                  {
-                      loader: 'css-loader',
-                      options: {
-                          modules: true
-                      }
-                  }
-              ]
-          }, {
-              test: /\.scss$/,
-              use: extractSass.extract({
-                  use: [{
-                      loader: "css-loader", options: {
-                          url: false
-                      }
-                  }, {
-                      loader: "sass-loader", options: {
-                        
-                      }
-                  }],
-                  // use style-loader in development
-                  fallback: "style-loader"
-              })
-          },
-          {
-              test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|png)(\?.*$|$)/,
-              use: [
-                  { loader: 'ignore-loader'}
-              ]
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
           }
-      ],
-
-    },
-    plugins: [
-        extractSass,
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                url: false
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {}
+            }
+          ],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|png)(\?.*$|$)/,
+        use: [{ loader: 'ignore-loader' }]
+      }
     ]
-};
+  },
+  plugins: [extractSass]
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
-var webpack = require('webpack');
-var config = require('./webpack.dev');
+var webpack = require('webpack')
+var config = require('./webpack.dev')
 
-config.output.filename = 'main.js';
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true, sourceMap: false}));
+config.output.filename = 'main.js'
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false }))
 
-module.exports = config;
+module.exports = config
