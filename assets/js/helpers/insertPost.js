74dc55d8f87d1ab7b73ab01acf258c28454bd2d3
Yaroslav Gribov <yaroslavgribov@yandex.ru>
2017-12-13 19:12:26 +0300

    update post feed

--- assets/js/helpers/insertPost.js
+++ assets/js/helpers/insertPost.js
@@ -1,43 +1,53 @@
 import { convertDate } from './convertDate'
 
 export function insertPost(postData, options) {
-  //start the inserting of the html
-  let postExcerpt = $(postData.html)
+  const { html, tags, image, url, title, published_at, author } = postData
+
+  const excerpt = $(html)
     .text()
     .split(' ')
     .slice(0, 20)
     .join(' ')
 
-  let tagText = options
+  const tagText = options
     ? options.includeTags
       ? `<span class="type"> 
-    ${postData.tags[0].name}
-    </span>`
+                    ${tags[0].name}
+                  </span>`
       : ''
     : ''
 
-  let classList = 'block post '
-  classList += options ? (options.addClass ? options.addClass : '') : ''
-  let date = options ? (options.includeDate ? `<p>${convertDate(postData.published_at)}</p>` : '') : ''
+  let classList = options ? (options.addClass ? options.addClass : '') : ''
+  let date = options ? (options.includeDate ? `<p>${convertDate(published_at)}</p>` : '') : ''
 
-  let imageContainer = postData.image
-    ? `<div class="rela post__image">
-      <img src="${postData.image}" /> 
-      ${tagText}
-    </div>`
-    : ''
-
-  let postContent = postData.html ? `<span class="text">${postExcerpt}...</span>` : ''
+  let imageContainer = ''
+  if (image) {
+    imageContainer = ``
+  }
 
   return `
-    <article class="${classList}">
-      <a href="${postData.url}">
-      ${imageContainer}
-      <h2 class="post-title">
-        ${postData.title}
-        ${date}
-      </h2>
-      ${postContent}
+    <article class="post-card ${classList}">
+    ${image &&
+      `<a class="post-card-image__link" href="${url}">
+        <div class="post-card__image" style="background-image: url(${image})"></div>
+      </a>`
+    }
+    <div class="post-card__content post-card-content">
+      <a class="post-card-content__link" href="${url}">
+        <header class="post-card__header">
+          <h2 class="post-card-title">
+            ${title}
+          </h2>
+        </header>
+        <section class="post-card__excerpt">
+          <p> ${excerpt} ... </p>
+        </section>
       </a>
-    </article>`
+      <footer class="post-card__meta">
+        ${author &&
+        `<a href="/author/${author.slug}">${author.name}</a>`
+        }
+      </footer>
+    </div>
+  </article>`
 }
