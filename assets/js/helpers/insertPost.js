abc49c9b473686c0534f10ab31636cdad36e587b
Yaroslav Gribov <yaroslavgribov@yandex.ru>
2018-02-05 16:21:37 +0300

    refactor post insertion

--- assets/js/helpers/insertPost.js
+++ assets/js/helpers/insertPost.js
@@ -1,51 +1,56 @@
-import { convertDate } from './convertDate';
+import templates from './templates'
 
-export function insertPost(postData, options) {
+export function insertPost(post, options) {
   //start the inserting of the html
-  const { primary_tag, feature_image } = postData;
+  const { primary_tag, feature_image, url, title, author } = post
 
-  const postExcerpt = $(postData.html)
+  const excerpt = post.custom_excerpt 
+  ? post.custom_excerpt 
+  : $(post.html)
     .text()
     .split(' ')
     .slice(0, 20)
-    .join(' ');
+    .join(' ')
 
-  const tagText = options
-    && options.includeTags
-    && primary_tag 
-    && `
-    <span class="type"> 
-      ${primary_tag.name}
-    </span>
-    `
-    || '';
-
-  let classList = 'block post ';
-  classList += options ? (options.addClass ? options.addClass : '') : '';
-  const date = options
-    ? options.includeDate ? `<p>${convertDate(postData.published_at)}</p>` : ''
-    : '';
-
-  const imageContainer = feature_image
-    ? `<div class="rela post__image">
-        <img src="${feature_image}" /> 
-        ${tagText}
-      </div>`
-    : '';
+  let classList = 'post post-card'
+  classList += options ? (options.addClass ? options.addClass : '') : ''
 
   // if post data contains html - show excerpt, if not - show expty string
-  const postContent = postData.html ? `<span class="text">${postExcerpt}...</span>` : '';
 
   return `
     <article class="${classList}">
-      <a href="${postData.url}">
-      ${imageContainer}
-      <h2 class="post-title">
-        ${postData.title}
-        ${date}
-      </h2>
-      ${postContent}
-      </a>
+      ${templates.if(options && options.includeTags && primary_tag, 
+        `<a class="tag" href="${primary_tag.url}"><span>${primary_tag.name}</span></a>`
+      )}
+      ${
+        templates.if(feature_image, `
+        <a class="post-card-image__link" href="${url}">
+          <div class="post-card__image" style="background-image: url(${feature_image})"></div>
+        </a>
+        `
+      )}
+      <div class="post-card__content post-card-content">
+        <a class="post-card-content__link" href="${url}">
+          <header class="post-card__header">
+            <h2 class="post-card-title">
+              ${title}
+            </h2>
+          </header>
+          ${
+            templates.if(post.html, `
+            <section class="post-card__excerpt">
+              <p> 
+                ${excerpt}
+              </p>
+            </section>
+            `
+          )}
+        </a>
+        <footer class="post-card__meta">
+          <a href=${author.url}>${author.name}</a>
+        </footer>
+      </div>
+      
     </article>
-  `;
+  `
 }
---
+++ assets/js/helpers/templates.js
@@ -0,0 +1,7 @@
+const templates = {
+  if(condition, thenTemplate, elseTemplate = '') {
+    return condition ? thenTemplate : elseTemplate
+  }
+}
+
+export default templates
