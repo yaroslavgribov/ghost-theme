bc9b9003dc3c44a07a3350ffb55810635c072d66
Yaroslav Gribov <yaroslav.gribov@iqoption.com>
2024-02-13 14:47:06 +0200

    CHORE- remove webpack

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('webpack')
-var config = require('./webpack.dev')
-
-config.output.filename = 'main.js'
-config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false })
-)
-
-module.exports = config
