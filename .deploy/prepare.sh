130510e3897d3cf4f4820119f5eb6150a88b4ebd
Yaroslav Gribov <yaroslav.gribov@iqoption.com>
2024-03-08 15:23:15 +0200

    issue-17 fix theme errors

--- .deploy/prepare.sh
+++ .deploy/prepare.sh
@@ -2,6 +2,7 @@ rm -rf public/*
 cp *.hbs public/
 cp package.json public/
 cp -r partials public/
+mkdir -p public/assets/build/
 cp -r assets/build public/assets/
 cp assets/*.png public/assets/
 ditto -c -k --sequesterRsrc --keepParent public public.zip
