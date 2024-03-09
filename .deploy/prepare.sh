29203e9e97b5bc2869104c9be1334b8bf958033e
Yaroslav Gribov <yaroslav.gribov@iqoption.com>
2024-03-09 17:24:45 +0200

    issue-19 fix feature image scaling

--- .deploy/prepare.sh
+++ .deploy/prepare.sh
@@ -4,5 +4,6 @@ cp package.json public/
 cp -r partials public/
 mkdir -p public/assets/build/
 cp -r assets/build public/assets/
+cp -r assets/icons public/assets/
 cp assets/*.png public/assets/
 ditto -c -k --sequesterRsrc --keepParent public public.zip
